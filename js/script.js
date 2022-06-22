7929054433e01272a3ffaa04785349a50477814b
Franpan94 <francescopanico94@libero.it>
2022-06-22 14:39:48 +0200

    html pagination

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let character = prompt('inserisci parola:')
+console.log(character);
 
 let reversecharacter;
 
@@ -16,3 +17,13 @@ while(i >= 0){
 }
 
 console.log(newcharacter);
+
+function characters(word, reverseword){
+    if(word === reverseword){
+        console.log('La parola è palindroma');
+    } else {
+        console.log('La parola non è palindroma');
+    }
+}
+
+characters(character, newcharacter);
